385002ac6bd48dfadb92d2f06e8cb2cb1477407d
prashanth459g <prashanth459g@gmail.com>
2022-01-07 16:16:41 +0530

    commit

--- js/javascript.js
+++ js/javascript.js
@@ -2,6 +2,5 @@ function hi() {
   alert("Welcome to Namma Kotturu");
   var name = prompt("please enter your name");
   document.write("Hi " + name + "<br> <br> My Account section is Coming Soon" +
-  "<br> <br> If you like to contribute to the project, Please visit *Contact Us* section of the website. <br> Thank you"
-+ "Or <a href="home.html#contact">Click hear</a>" );
+  "<br> <br> If you like to contribute to the project, Please visit *Contact Us* section of the website. <br> Thank you <br> <br> ");
 }
