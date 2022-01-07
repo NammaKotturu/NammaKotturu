dae052d0b58d7af90ba724cbc28c75c78f188561
prashanth459g <prashanth459g@gmail.com>
2022-01-07 14:03:46 +0530

    updated template.html

---
+++ js/javascript.js
@@ -0,0 +1,6 @@
+function hi() {
+  alert("Welcome to Namma Kotturu");
+  var name = prompt("please enter your name");
+  document.write("Hi " + name + "<br> <br> My Account section is Coming Soon" +
+  "<br> <br> If you like to contribute to the project, Please visit *Contact Us* section of the website. <br> Thank you" );
+}
